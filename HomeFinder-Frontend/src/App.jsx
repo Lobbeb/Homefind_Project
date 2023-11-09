0ea8522e929699efe8369c5de57787c10edd5bc7
Lobbe <Lobbeolsson@gmail.com>
2023-11-09 09:40:31 +0100

    Created some of the routes to the base pages

--- HomeFinder-Frontend/src/App.jsx
+++ HomeFinder-Frontend/src/App.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/about";
 
 export default function App() {
-  return <h1 className="text-red-300">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
