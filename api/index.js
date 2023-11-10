fbdd0f5278080c701013e1b853fb0c02b20255d4
Lobbe <Lobbeolsson@gmail.com>
2023-11-10 13:09:48 +0100

    Connecting to our database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+/*Connect to server, hiding server password */
+/*We do not know if we are connected to not so we add a "then" and try to catch an error if were not -> FEEDBACK*/
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
