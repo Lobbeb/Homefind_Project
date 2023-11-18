f566a9d72a69d6753755325fb6602644aecc15e2
Lobbe <Lobbeolsson@gmail.com>
2023-11-18 11:55:53 +0100

    Temp profile page fixed

--- HomeFinder-Frontend/src/pages/Profile.jsx
+++ HomeFinder-Frontend/src/pages/Profile.jsx
@@ -1,5 +1,42 @@
-import React from "react";
+import { useSelector } from "react-redux";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div
+      className="p-6 max-w-lg mx-auto my-8 rounded-lg shadow"
+      style={{ backgroundColor: "rgba(229, 231, 235, 0.85)" }}
+    >
+      <h1 className="text-3xl font-bold text-center my-8">Profile Page</h1>
+      <form className="flex flex-col gap-3">
+        <img
+          src={currentUser.avatar}
+          alt="profile"
+          className="rounded-full w-20 h-20 object-cover cursor-pointer self-center mt-3 mb-3"
+        />
+        <input
+          type="text "
+          placeholder="Username"
+          className="border p-3 rounded-lg"
+        ></input>
+        <input
+          type="text "
+          placeholder="Password"
+          className="border p-3 rounded-lg"
+        ></input>
+        <input
+          type="text "
+          placeholder="Email"
+          className="border p-3 rounded-lg"
+        ></input>
+        <button className="bg-blue-700 text-white font-semibold rounded-lg p-3 uppercase hover:opacity-90 disabled:bg-gray-500 disabled:hover:opacity-100">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="cursor-pointer hover:underline">delete account</span>
+        <span className="cursor-pointer hover:underline">sign out</span>
+      </div>
+    </div>
+  );
 }
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,77 +3,75 @@ import bcryptjs from "bcryptjs"; // Import bcryptjs for password hashing
 import { errorHandler } from "../utils/error.js"; // Import error handler
 import jwt from "jsonwebtoken"; // Import jsonwebtoken for creating tokens
 
-// TODO: Add First & last - name to the signup, potential add more stuff
+// Signup controller
 export const signup = async (req, res, next) => {
-  // ADD THINGS BELOW FOR SIGNUP RIGHT NOW ONLY USERNAMNE EMAIL AND PASSWORD
-  // Signup controller
-  const { username, email, password } = req.body; // Get username, password and email from request body
-  const hashedPassword = bcryptjs.hashSync(password, 10); // Hash the password
-
-  const newUser = new User({ username, email, password: hashedPassword }); // Create a new user
   try {
-    // Try to save the new user
+    const { username, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
-    res.status(201).json("user created succesfully"); // Send a response
-  } catch (error) {
-    next(error); // Send error to error handler
-  }
-};
 
-//Sign in function
-
-export const signin = async (req, res, next) => {
-  const { email, password } = req.body; // Get email and password from request body
-  try {
-    const validUser = await User.findOne({ email }); // Find the user by email
-    if (!validUser) return next(errorHandler(404, "user not found")); // If user not found, send error
-
-    const validPassword = bcryptjs.compareSync(password, validUser.password); // Compare the passwords <- because we hashed it
-    if (!validPassword) return next(errorHandler(401, "Wrong password")); // If password is invalid, send error
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET); // Create a token
-    const { password: pass, ...rest } = validUser._doc; // Get all the user data except password
-    res
-      .cookie("access_token", token, { httpOnly: true })
-      .status(200)
-      .json(rest); // Send token as cookies and user data as response
+    res.status(201).json("User created successfully");
   } catch (error) {
     next(error);
   }
 };
 
+// Signin controller
+export const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) return next(errorHandler(404, "User not found"));
+
+    const isPasswordValid = bcryptjs.compareSync(password, user.password);
+    if (!isPasswordValid) return next(errorHandler(401, "Wrong password"));
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const { password: pass, ...userData } = user._doc;
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(userData);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Google authentication controller
 export const google = async (req, res, next) => {
   try {
-    // Find the user by email aka google account
-    const user = await User.findOne({ email: req.body.email });
-    if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET); // Create a token
-      const { password: pass, ...rest } = user._doc; // Get all the user data except password
-      res
-        .cookie("access_token", token, { httpOnly: true })
-        .status(200)
-        .json(rest);
-    } else {
-      // Create a new user if you dont have google account
+    const { email, name, photo } = req.body;
+    let user = await User.findOne({ email });
+
+    if (!user) {
       const generatedPassword =
-        Math.random().toString(36).slice(-8) +
-        Math.random().toString(36).slice(-8);
-      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10); // Hash the password
-      const newUser = new User({
+        Math.random().toString(36).slice(-5) +
+        Math.random().toString(36).slice(-5);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+
+      user = new User({
         username:
-          req.body.name.split(" ").join("").toLowerCase() +
+          name.split(" ").join("").toLowerCase() +
           Math.random().toString(36).slice(-5),
-        email: req.body.email,
+        email: email,
         password: hashedPassword,
-        avatar: req.body.photo,
+        avatar: photo,
       });
-      await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET); // Create a token
-      const { password: pass, ...rest } = newUser._doc; // Get all the user data except password
-      res
-        .cookie("access_token", token, { httpOnly: true })
-        .status(200)
-        .json(rest);
+
+      await user.save();
     }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const { password: pass, ...userData } = user._doc;
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(userData);
   } catch (error) {
     next(error);
   }
