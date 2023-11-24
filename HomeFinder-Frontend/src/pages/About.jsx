0e3cd9001737fb951c00b98c2012b258e30d101a
Lobbe <Lobbeolsson@gmail.com>
2023-11-24 15:51:28 +0100

    Added about page (THE CORE IS FINISHED)

--- HomeFinder-Frontend/src/pages/About.jsx
+++ HomeFinder-Frontend/src/pages/About.jsx
@@ -1,5 +1,33 @@
-import React from "react";
-
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto bg-blue-900 rounded-lg bg-opacity-95 my-20 ">
+      <h1 className="text-3xl font-bold mb-4 text-white">About Homefind</h1>
+      <p className="mb-4 text-white">
+        At Homefind, we're pioneering a new approach to property search that
+        prioritizes the immediate surroundings of your potential new home. Our
+        innovative platform is designed to refine and tailor the apartment
+        search experience to suit your unique needs.
+      </p>
+      <p className="mb-4 text-white">
+        Our intuitive website allows you to filter your search based on a
+        variety of self-selected criteria, ensuring you find a place that truly
+        feels like home. With our map-based search tool, you'll be able to
+        visualize potential homes within the context of their neighborhoods,
+        allowing you to consider the Points of Interest in the vicinity and
+        discover what's nearby without the need for extensive research.
+      </p>
+      <p className="mb-4 text-white">
+        We're not just about helping you find a new property; we aim to find the
+        best home for you. With enough input data, our system can recommend
+        properties that best match your preferences, making the search process
+        more enjoyable and time-efficient. And for the homes you love, you can
+        save them to your favorites for future reference.
+      </p>
+      <p className="mb-4 text-white">
+        Join us at Homefind, where your journey to the perfect home begins with
+        us by your side. We're more than just a real estate agency; we're your
+        partner in making your dream home a reality.
+      </p>
+    </div>
+  );
 }
